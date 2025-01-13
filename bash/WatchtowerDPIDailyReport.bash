bebe3566b638ce831d7187ca2363245a23ba082e
Zachary Pax <157398277+ZacharyPax@users.noreply.github.com>
2025-01-13 07:48:55 -0600

    another fix attempt

--- bash/WatchtowerDPIDailyReport.bash
+++ bash/WatchtowerDPIDailyReport.bash
@@ -20,7 +20,7 @@ echo "|   Removing files from holding cell...                                |"
 rm ~/Watchtower-Reporting/Holding-Cell/*
 echo "|   Preparing log...                                                   |"
 echo ------------------------------------------------------------------------
-/usr/bin/python3 ./trace-summary -b -n 75 -c ~/Watchtower-Reporting/WatchtowerDPIDailyLog.log
+/usr/bin/python3 ~/Watchtower-Reporting/trace-summary -b -n 75 -c ~/Watchtower-Reporting/WatchtowerDPIDailyLog.log
 echo ------------------------------------------------------------------------
 echo -   Cleaning up...   -
 rm WatchtowerDPIDailyLog.log
