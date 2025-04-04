4259b0818fdc4a087eee9ff8c2e0c6c3bf624c66
Zachary Pax <157398277+ZacharyPax@users.noreply.github.com>
2025-04-04 07:25:20 -0500

    call nisc discord script when borking is detected

--- bash/check-nisc.bash
+++ bash/check-nisc.bash
@@ -19,6 +19,7 @@ wget -q -O /tmp/nongreen.html "$URL"
 if ! grep -q "All Monitored Systems OK" /tmp/nongreen.html; then
   echo "The NISC server did not return the expected response from Xymon to hezekiah.vm.wt.local! Check if the server is okay! This script normally runs every minute, but will now sleep for 45 minutes to prevent alert fatigue." | mail -s "[WATCHTOWER NISC MONITORING] NISC may be borked!" -r SENDER@address.com destination@address.com destination2@address.com
   echo "Sleeping for 2700 seconds..."
+  bash nisc-discord.bash
   sleep 2700
 fi
 
